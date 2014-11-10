b3cc0c7a7a1c30d5e227fddea8a0d069bed217bf
Kristof Jozsa <kristof.jozsa@gmail.com>
2014-11-11 00:10:47 +0100

    load twits on demand

--- twits-web/src/main/scala/twits/snippet/Twits.scala
+++ twits-web/src/main/scala/twits/snippet/Twits.scala
@@ -1,5 +1,6 @@
 package twits.snippet
 
+import net.liftweb.http.SHtml
 import net.liftweb.util.Helpers._
 import twitter4j.{Paging, TwitterFactory}
 
@@ -10,21 +11,30 @@ import scala.xml._
 class Twits {
   val twitter = TwitterFactory.getSingleton()
 
-  def timeline = Stream from (1) takeWhile (_ <= 2) flatMap { page => twitter.getUserTimeline(new Paging(page))}
+  var pageSize = 10
+
+  def timeline = Stream from (1) flatMap { page => twitter.getUserTimeline(new Paging(page))}
 
   def prettify(message: String): NodeSeq = message.split(" ").foldLeft(NodeSeq.Empty) { (acc, s) =>
     val node = s match {
-      case s if s.startsWith("#") => <span class="bg-success">{s}</span>
-      case s if s.startsWith("@") => <span class="bg-danger">{s}</span>
+      case s if s.startsWith("#") => <span class="bg-success">
+        {s}
+      </span>
+      case s if s.startsWith("@") => <span class="bg-danger">
+        {s}
+      </span>
       case s => Text(s)
     }
     acc ++ node ++ Text(" ")
   }
 
   def render = {
-    ".twit *" #> timeline.view.zipWithIndex.map { case (twit, idx) =>
-      ".num" #> (1 + idx) &
-      ".message *" #> prettify(twit.getText)
-    }
+    SHtml.idMemoize(outer =>
+      ".twit *" #> timeline.view.take(pageSize).zipWithIndex.map { case (twit, idx) =>
+        ".num" #> (1 + idx) &
+          ".message *" #> prettify(twit.getText)
+      } &
+        ".more" #> SHtml.a(() => { pageSize += 10; outer.setHtml() }, <p>load more</p>)
+    )
   }
 }
